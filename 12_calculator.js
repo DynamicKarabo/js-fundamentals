22a3a904bf00a692006345f899f5eea05a08fa18
Karabo Oliphant🧬 <karabooliphant34@gmail.com>
2025-12-15 12:40:38 +0200

    Refactor calculator to perform basic arithmetic
    Removed duplicate filtering logic and added basic arithmetic operations based on the selected operator.

---
+++ 12_calculator.js
@@ -0,0 +1,15 @@
+let a = 10;
+let b = 5;
+let op = "+"; // change to -, *, /
+
+if (op === "+") {
+  console.log(a + b);
+} else if (op === "-") {
+  console.log(a - b);
+} else if (op === "*") {
+  console.log(a * b);
+} else if (op === "/") {
+  console.log(a / b);
+} else {
+  console.log("Invalid operator");
+}
