66edb50d4db32d18ce58c13822641c3e2a75e427
Karabo Oliphant🧬 <karabooliphant34@gmail.com>
2025-12-15 12:38:38 +0200

    Implement function to remove duplicates from array

---
+++ 11_remove_duplicates.js
@@ -0,0 +1,10 @@
+let arr = [1, 2, 2, 3, 4, 4, 5];
+let result = [];
+
+for (let i = 0; i < arr.length; i++) {
+  if (!result.includes(arr[i])) {
+    result.push(arr[i]);
+  }
+}
+
+console.log(result);
